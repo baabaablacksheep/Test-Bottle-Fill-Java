d9d1df940bca2335e7180805a5b1ba3446dd8f0b
baabaablacksheep <sahanpoornaka1@gmail.com>
2018-09-12 00:34:14 +0530

    update version 0.21

--- src/Demo.java
+++ src/Demo.java
@@ -1,26 +1,27 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import javax.swing.*;
 
 public class Demo {
+    public static JButton button= new JButton();
+    public static JTextField textField = new JTextField();
     public static void main(String[] args) {
         JFrame frame = new JFrame("JFrame Example");
 
         JPanel panel = new JPanel();
         panel.setLayout(null);
-//        panel.setLayout(new GridLayout());
-//        panel.setLayout(new FlowLayout());
 
-        JButton button = new JButton();
         button.setText("Press Me");
         button.setBounds(100,100,100,20);
 
-        JTextField textField=new JTextField();
+        textField=new JTextField();
         textField.setBounds(50,50, 150,20);
 
 
-        button.addActionListener(e -> textField.setText("Welcome to The Jungle"));
+        //button.addActionListener(e -> textField.setText("Welcome to The Jungle"));
 
         panel.add(button);
         panel.add(textField);
@@ -32,5 +33,38 @@ public class Demo {
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+    }
+}
+
+class MouseObserver implements MouseListener {
+
+    MouseObserver(){
+        Demo.button.addMouseListener(this);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        Demo.textField.setText("Mouse Clicked.........");
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        Demo.textField.setText("Mouse Released.........");
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
 }
