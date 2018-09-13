17fe8fd76a297dfb4a0f98d0e152d20b34c2c3dc
sahan.d <sahan.d@creativesoftware.com>
2018-09-13 19:21:21 +0530

    Added the Flask Image Update 2.34

--- src/Demo.java
+++ src/Demo.java
@@ -1,27 +1,16 @@
+import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.awt.Graphics;
 
-public class Demo implements MouseListener{
+public class Demo implements MouseListener {
 
     private static JButton fillButton = new JButton();
     private static JButton drainButton = new JButton();
     private static JTextField textField = new JTextField();
-    private BufferedImage flaskImage;
+    private static JLabel flaskImageLabel;
+    private static ImageIcon flaskImage;
 
-    Demo() {
-        try {
-            flaskImage = ImageIO.read(new File("/resources/flask.png"));
-
-        } catch (IOException ex) {
-            // handle exception...
-        }
-    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Bottle Fill Example");
@@ -29,30 +18,30 @@ public class Demo implements MouseListener{
         JPanel panel = new JPanel();
         panel.setLayout(null);
 
+        flaskImage = new ImageIcon("./resources/flask.png");
+        flaskImageLabel = new JLabel(flaskImage);
+        flaskImageLabel.setBounds(0,0,628,720);
+
         fillButton.setText("Fill");
-        fillButton.setBounds(50,100,80,20);
+        fillButton.setBounds(50, 750, 80, 20);
 
         drainButton.setText("Drain");
-        drainButton.setBounds(200,100,80,20);
+        drainButton.setBounds(200, 750, 80, 20);
 
         fillButton.addMouseListener(new Demo());
         drainButton.addMouseListener(new Demo());
 
-        textField=new JTextField();
-        textField.setBounds(50,50, 250,20);
+        textField = new JTextField();
+        textField.setBounds(50, 50, 250, 20);
 
+        panel.add(flaskImageLabel);
         panel.add(fillButton);
         panel.add(drainButton);
         panel.add(textField);
 
-/*        @Override
-        protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            g.drawImage(image, 0, 0, this); // see javadoc for more info on the parameters
-        }*/
 
         frame.add(panel);
-        frame.setSize(800, 640);
+        frame.setSize(630, 800);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
@@ -65,10 +54,9 @@ public class Demo implements MouseListener{
 
     @Override
     public void mousePressed(MouseEvent e) {
-        if(e.getSource().equals(fillButton)){
+        if (e.getSource().equals(fillButton)) {
             textField.setText("Mouse Pressed By Fill!");
-        }
-        else{
+        } else {
             textField.setText("Mouse Pressed By Drain!");
         }
 
@@ -76,10 +64,9 @@ public class Demo implements MouseListener{
 
     @Override
     public void mouseReleased(MouseEvent e) {
-        if(e.getSource().equals(fillButton)){
+        if (e.getSource().equals(fillButton)) {
             textField.setText("Mouse Released By Fill!");
-        }
-        else{
+        } else {
             textField.setText("Mouse Released By Drain!");
         }
 
@@ -87,10 +74,9 @@ public class Demo implements MouseListener{
 
     @Override
     public void mouseEntered(MouseEvent e) {
-        if(e.getSource().equals(fillButton)){
+        if (e.getSource().equals(fillButton)) {
             textField.setText("Mouse Has Entered to Fill!");
-        }
-        else{
+        } else {
             textField.setText("Mouse Has Entered to Drain!");
         }
 
@@ -98,10 +84,9 @@ public class Demo implements MouseListener{
 
     @Override
     public void mouseExited(MouseEvent e) {
-        if(e.getSource().equals(fillButton)) {
+        if (e.getSource().equals(fillButton)) {
             textField.setText("Mouse Has Gone Away From Fill");
-        }
-        else{
+        } else {
             textField.setText("Mouse Has Gone Away From Drain");
         }
     }
